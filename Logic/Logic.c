e8cbe27065f253b43dd96a9e22b71923fac3e25f
Christian Andersen <s133288@dtu.dk>
2022-05-02 15:34:17 +0200

    more refactoring

--- Logic/Logic.c
+++ Logic/Logic.c
@@ -9,24 +9,116 @@
 #include "../Data/Data_Header.h"
 #include "../Interface/Display_Header.h"
 #include "../Interface/Reader_Header.h"
-#include "Header.h"
 
 //own methods declaration
-void deAllocateMalloc();
 void processPlayerInput(char*);
+void shuffle();
+void saveGame(char*);
+void setupBoard();
 
-/**
- *
- */
+void deAllocateMalloc();
+
 extern void playGame() {
-    updateDisplay();
-
+    updateDisplay();    //just to create a display the first time
     do {
         processPlayerInput(dataPTR_lastCommand());
         updateDisplay();
-
     }while(!(*dataPTR_IsGameDone()));
 }
+
+/**
+ * Author: Frederik G. Petersen (S215834)
+ * @param string
+ */
+// TODO Make better initial comparison to check nothing follows the initials
+void processPlayerInput(char* string){
+
+    if (strcasecmp(string, "QQ\n") == 0) {
+        remove("..\\CurrentSeed.txt");
+        if (dataPTR_DeckLoaded())
+            deAllocateMalloc();
+        printf("\n---Exiting Game---\n");
+        exit(0);
+    }
+
+    if (!(*dataPTR_DeckLoaded())) {
+        if (strstr(string, "LD") != NULL)
+            setupCards(dataPTR_lastCommand());
+        else
+            setErrorMessage("Wrong game command at current stage of the game");
+    }
+
+    else if  (!(*dataPTR_GameStarted())) {
+
+        if (strcasecmp(string, "SW\n") == 0) {
+            //All functionality of this one is programmed into the display.
+            setErrorMessage("OK");
+        }
+
+        else if (strcasecmp(string, "SI\n") == 0) {
+            //todo, do something
+        }
+
+        else if (strcasecmp(string, "SR\n") == 0) {
+            if (dataPTR_DeckLoaded()) {
+                shuffle();
+                setErrorMessage("OK");
+            } else {
+                setErrorMessage("You must load a deck before shuffeling");
+            }
+        }
+
+        else if (strstr(string,"SD") != NULL) {
+            if (strlen(string) > 3) {
+                saveGame(&string[3]);
+            } else
+                setErrorMessage("You must choose a file name");
+        }
+
+        else if (strcasecmp(string, "P\n") == 0) {
+            saveGame("CurrentSeed.txt"); //saves the current card setup.
+            setupBoard();
+            *dataPTR_GameStarted() = true;
+            setErrorMessage("OK");
+        }
+
+        else
+            setErrorMessage("Wrong game command at current stage of the game");
+    }else if (dataPTR_GameStarted()) {
+        if (strcasecmp(string, "Q\n") == 0){             //ensures that a game is in progress.
+            setErrorMessage("OK");
+            deAllocateMalloc();                                 //removes the current card, and free the memory.
+            setupCards("LD ../currentSeed.txt");                                       //Setup 1 LinkedList containing the cards from CurrentSeed.txt
+            *dataPTR_GameStarted() = false;
+        }
+
+        if (string[2] == ':' && string[5] == '-' && string[6] == '>'){ // Game Move
+            // Get everything from input
+            char fromColumn[3];
+            char card[3];
+            char toColumn[3];
+
+            memcpy(fromColumn, string, 2);
+            memcpy(card, &string[3], 2);
+            memcpy(toColumn, &string[7], 2);
+
+            card[2] = '\0';
+            toColumn[2] = '\0';
+
+            // Process it
+            //bool moveMade = attemptCardMove(fromColumn, card, toColumn);
+            //TODO make PlayGame handle error messages
+        }
+
+        else
+            setErrorMessage("Wrong game command at current stage of the game");
+
+    }else {
+        //shouldn't happend
+        //could implement some reset code here to reset the entire game
+    }
+}
+
 
 /**
  *
@@ -42,9 +134,9 @@ bool winCondition(){
  * Shuffle the deck
  */
 void shuffle() {
-    LinkedList* deck = dataPTR_ToDeck();       //gets a pointer to the current LinkedList
-    Card* nextCard = deck->head->next;  //keeps a track of the remaining card we need to shuffle into the LinkedList
-    int cardInList = 1;                 //keep a track of how many cards is currently in the new LinkedList
+    LinkedList* deck = dataPTR_ToDeck();    //gets a pointer to the current LinkedList
+    Card* nextCard = deck->head->next;      //keeps a track of the remaining card we need to shuffle into the LinkedList
+    int cardInList = 1;                     //keep a track of how many cards is currently in the new LinkedList
 
     //removes all the card except the first from the LinkedList, and ensure all pointers are correct.
     deck->tail = deck->head;
@@ -158,115 +250,6 @@ void saveGame(char* filename){
         }
     }
     fclose(fp);
-}
-
-/**
- * Author: Frederik G. Petersen (S215834)
- * @param string
- */
- // TODO Make better initial comparison to check nothing follows the initials
-void processPlayerInput(char* string){
-
-    if (string[2] == ':' && string[5] == '-' && string[6] == '>'){ // Game Move
-        // Get everything from input
-        char fromColumn[3];
-        char card[3];
-        char toColumn[3];
-
-        memcpy(fromColumn, string, 2);
-        memcpy(card, &string[3], 2);
-        memcpy(toColumn, &string[7], 2);
-
-        card[2] = '\0';
-        toColumn[2] = '\0';
-
-        // Process it
-        //bool moveMade = attemptCardMove(fromColumn, card, toColumn);
-        //TODO make PlayGame handle error messages
-
-    } else {
-        char initials[3];
-        memcpy(initials, string, 2);
-
-        // Things to process
-        if (strcasecmp(initials, "LD") == 0){
-            if (!(*dataPTR_DeckLoaded()))
-                setupCards(dataPTR_lastCommand());
-            else
-                setErrorMessage("Deck already loaded");
-        } else if (strcasecmp(initials, "SW") == 0){
-            //All functionality of this one is programmed into the display.
-            setErrorMessage("OK");
-        } else if (strcasecmp(initials, "SI") == 0){
-
-        } else if (strcasecmp(initials, "SR") == 0){
-            if (dataPTR_DeckLoaded()) {
-                shuffle();
-                setErrorMessage("OK");
-            } else {
-                setErrorMessage("You must load a deck before shuffeling");
-            }
-
-        } else if (strcasecmp(initials, "SD") == 0){
-            if (dataPTR_DeckLoaded()) {
-                if(strlen(string) > 3) {
-                    char *filename = &string[3];
-                    saveGame(filename);
-                } else
-                    setErrorMessage("You must choose a file name");
-            }else
-                setErrorMessage("You must first load i file to save it");
-        } else if (strcasecmp(initials, "QQ") == 0){
-            remove("..\\CurrentSeed.txt");
-            if(dataPTR_DeckLoaded())
-                deAllocateMalloc();
-            printf("\n---Exiting Game---\n");
-            exit(0);
-
-        // if a deck is loaded and the game is not already in progress, this will build our 11 LinkedList
-        // for the board, and update the game state.
-        }else if (strcasecmp(initials, "P\n") == 0){
-            if(*dataPTR_GameStarted())
-                setErrorMessage("Game already in progress");
-            else if(dataPTR_DeckLoaded()) {
-                saveGame("CurrentSeed.txt"); //saves the current card setup.
-                setupBoard();
-
-                /*
-                // TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!1
-                // Find better place for this:
-                attemptMovingCardsToFoundation(&dataPTR_ToBoard()[0]);
-                attemptMovingCardsToFoundation(&dataPTR_ToBoard()[1]);
-                attemptMovingCardsToFoundation(&dataPTR_ToBoard()[2]);
-                attemptMovingCardsToFoundation(&dataPTR_ToBoard()[3]);
-                attemptMovingCardsToFoundation(&dataPTR_ToBoard()[4]);
-                attemptMovingCardsToFoundation(&dataPTR_ToBoard()[5]);
-                attemptMovingCardsToFoundation(&dataPTR_ToBoard()[6]);
-                 */
-
-                *dataPTR_GameStarted() = true;
-                setErrorMessage("OK");
-            }else{
-                setErrorMessage("Must load a deck, before you can start the game");
-            }
-
-        // If the game is running, this will reverse the gamestate, and load the card into a single LinkedList
-        // with the same setup as before the game started.
-        } else if (strcasecmp(initials, "Q\n") == 0){
-            if (*dataPTR_GameStarted()) {                                 //ensures that a game is in progress.
-                setErrorMessage("OK");
-                deAllocateMalloc();                                 //removes the current card, and free the memory.
-                setupCards("LD ../currentSeed.txt");                                       //Setup 1 LinkedList containing the cards from CurrentSeed.txt
-                *dataPTR_GameStarted() = false;
-            }
-            else
-                setErrorMessage("Game not started");
-
-        //the default if everything else fails.
-        } else {
-            setErrorMessage("Invalid command, try again");
-        }
-    }
 }
 
 
