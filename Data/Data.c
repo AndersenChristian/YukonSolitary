b7e50617c8750e58ad2a91f5a12c967d18706622
Christian Andersen <s133288@dtu.dk>
2022-05-05 23:00:36 +0200

    moved 3 methods into Data

--- Data/Data.c
+++ Data/Data.c
@@ -1,9 +1,10 @@
+//Libaries
 #include "Data_Header.h"
 #include <stdbool.h>
-#include <stdlib.h>
 #include <string.h>
+#include <malloc.h>
 
-LinkedList deck = {0, NULL, NULL};
+LinkedList deck;
 LinkedList boardSlots[11]; //0-6 Cards, 7-11 Foundations
 
 char lastCommand[255] = "\n";
@@ -11,7 +12,6 @@ char errorMessage[255]; //set by the various methods of the program to proper di
 
 //the following boolean values are used to keep a track of the current game-state
 bool deckLoaded = false;
-bool isGameDone = false;
 bool gameStarted = false;
 
 LinkedList* dataPTR_ToDeck(){
@@ -34,14 +34,83 @@ bool* dataPTR_DeckLoaded(){
     return &deckLoaded;
 }
 
-bool* dataPTR_IsGameDone(){
-    return &isGameDone;
-}
-
 bool* dataPTR_GameStarted(){
     return &gameStarted;
 }
 
 void setErrorMessage(char* message){
     strcpy(errorMessage, message);
+}
+
+
+/**
+ * Author: Frederik G. Petersen (S215834)
+ * @param list
+ * @param pCard
+ */
+void addCard(const char* cardInfo){
+    LinkedList* list = dataPTR_ToDeck();
+    Card* newCard = malloc(sizeof (Card));
+    newCard->name[0] = cardInfo[0];
+    newCard->name[1] = cardInfo[1];
+    newCard->name[2] = '\0';
+    newCard->next = NULL;
+    newCard->prev = list->tail;
+    newCard->faceUp = false;
+
+    if (list->head == NULL){ // First entry
+        list->head = newCard;
+        list->tail = newCard;
+
+    } else { // Every other entry
+        Card* oldTail = list->tail;
+
+        oldTail->next = newCard;
+
+        newCard->prev = oldTail;
+        newCard->next = NULL;
+
+        list->tail = newCard;
+    }
+}
+
+/**
+ * Author: Frederik G. Petersen (S215834)
+ *
+ * @param card gets the numerical value of a Card.
+ * @return numerical value of the card. Returns 0 when passed NULL
+ */
+int getCardValue(Card* card){
+    if (card==NULL) return 0;
+    switch (card->name[0]) {
+        case 'A': return 1;
+        case '2': return 2;
+        case '3': return 3;
+        case '4': return 4;
+        case '5': return 5;
+        case '6': return 6;
+        case '7': return 7;
+        case '8': return 8;
+        case '9': return 9;
+        case 'T': return 10;
+        case 'J': return 11;
+        case 'Q': return 12;
+        case 'K': return 13;
+    }
+}
+
+/**
+ * Author: Frederik G. Petersen (S215834)
+ *
+ * Extracts the card's suit from the name of a card
+ * @param card
+ * @return ENUM of Card_Suits
+ */
+CARD_SUITS getCardSuit(Card* card){
+    switch (card->name[1]) {
+        case 'S': return S;
+        case 'D': return D;
+        case 'H': return H;
+        case 'C': return C;
+    }
 }
