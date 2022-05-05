66f8c3e1d62cf484d01ee3600ecaf68d3440b1cd
Christian Andersen <s133288@dtu.dk>
2022-05-05 23:01:36 +0200

    Updated header to only include global methods, rest is local declared inside the class itself

--- Logic/GameController_Header.h
+++ Logic/GameController_Header.h
@@ -1,17 +1,6 @@
 #ifndef YUKON_GAMECONTROLLER_HEADER_H
 #define YUKON_GAMECONTROLLER_HEADER_H
 
-#include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include "../Data/Data_Header.h"
-
 void gameMove(char* input);
-void addCard(const char*);
-Card* getCardByName(LinkedList* list, char* name);
-Card* getLastCard(LinkedList* list);
-CARD_SUITS getCardSuit(Card*);
-int getCardValue(Card*);
-void printList(LinkedList* list);
 
 #endif //YUKON_GAMECONTROLLER_HEADER_H
